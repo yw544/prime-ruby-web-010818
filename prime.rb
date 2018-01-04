1442df65593489a9b41646788dcb1918866bcb41
York Wang <yw544@nyu.edu>
2018-01-04 18:56:51 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
 # Add  code here!
+def prime?(number)
+  return false if num <=1
+  (2..Math.sqrt(number)).each do |number2|
+    if number % number 2 == 0 
+      return false
+    end
+
+  end
+  return true
+end
+  
+  
